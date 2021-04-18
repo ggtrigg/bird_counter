6b74d9c7b2562d97eb2660c6ac7335089c19b1ea
Glenn Trigg <ggtrigg@gmail.com>
2021-04-18 16:32:18 +1000

    Use a drawing area instead of an image so the pixbuf will resize when the window resizes.

--- src/gui.rs
+++ src/gui.rs
@@ -1,14 +1,16 @@
 extern crate gtk;
 extern crate gdk;
+extern crate cairo;
 
 use crate::Animal;
 use directories_next::ProjectDirs;
 use gtk::prelude::*;
+use gdk::prelude::*;
 use gtk::{
     Box, Entry, EventBox, FileChooserAction, FileChooserDialog, FileFilter, Image, MessageDialog,
-    Orientation, ResponseType, Window,
+    Orientation, ResponseType, Window, DrawingArea
 };
-use gdk::EventConfigure;
+use gdk::{EventConfigure};
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
@@ -56,9 +58,9 @@ fn animal_selected(object: &EventBox, _event: &gdk::EventButton) -> gtk::Inhibit
     if let Some(animal_id) = unsafe { object.get_data::<i64>("animal") } {
         if *animal_id != 0 {
             if let Some(child) = object.get_child() {
-                if let Ok(image) = child.downcast::<gtk::Image>() {
+                if let Ok(da) = child.downcast::<gtk::DrawingArea>() {
                     crate::log_sighting(*animal_id);
-                    add_tick(&image);
+                    da.queue_draw();
                 }
             }
         } else {
@@ -132,45 +134,47 @@ fn animal_selected(object: &EventBox, _event: &gdk::EventButton) -> gtk::Inhibit
     Inhibit(true)
 }
 
-fn load_image(animal: Option<&Animal>) -> gtk::Image {
-    let img = match animal {
-        Some(animal) => match &animal.filename {
-            Some(filename) => Image::from_file(image_dir().join(Path::new(filename))),
-            None => Image::from_file(image_dir().join(Path::new("unknown.png"))),
-        },
-        None => Image::from_file(image_dir().join(Path::new("add_new.png"))),
-    };
-    if let Some(source_pb) = img.get_pixbuf() {
-        let sw = source_pb.get_width();
-        let sh = source_pb.get_height();
-        let mut dw = 180;
-        let mut dh = 180;
-        if sw > sh {
-            dh = 180 * sh / sw;
-        } else if sh > sw {
-            dw = 180 * sw / sh;
-        }
-        img.set_from_pixbuf(
-            source_pb
-                .scale_simple(dw, dh, gdk_pixbuf::InterpType::Bilinear)
-                .as_ref(),
-        );
-    }
-    let today_count = select!(i64 "count(*) from sighting where animal_id = ? and date(seen_at, \"unixepoch\", \"localtime\") = date(\"now\")",
-    match animal {
-        Some(animal) => animal.rowid.unwrap_or(0),
-        None => 0
-    }).unwrap_or(0);
-    if today_count > 0 {
-        add_tick(&img);
-    }
-    img
+fn load_image(_animal: Option<&Animal>) -> gtk::DrawingArea {
+    let da = DrawingArea::new();
+    da.set_size_request(180, 180);
+    da.connect_draw(draw_image);
+    da
 }
 
-fn add_tick(image: &gtk::Image) {
+fn draw_image(da: &gtk::DrawingArea, context: &cairo::Context) -> gtk::Inhibit {
+    if let Some(eventbox) = da.get_parent() {
+        if let Some(animal_id) = unsafe { eventbox.get_data::<i64>("animal") } {
+            let pb = get_animal_pixbuf(animal_id, da.get_allocated_width(), da.get_allocated_height());
+            context.set_source_pixbuf(&pb, 0.0, 0.0);
+            context.paint();
+        }
+    }
+    Inhibit(false)
+}
+
+fn get_animal_pixbuf(animal_id: &i64, width: i32, height: i32) -> gdk_pixbuf::Pixbuf {
+    let mut pb = gdk_pixbuf::Pixbuf::from_file_at_scale(image_dir().join(Path::new("unknown.png")), width, height, true)
+        .expect("Error loading file into pixbuf");
+
+    if let Ok(animal) = select!(Animal "where rowid = ?", animal_id) {
+        let res = match &animal.filename {
+            Some(filename) => gdk_pixbuf::Pixbuf::from_file_at_scale(image_dir().join(Path::new(filename)), width, height, true),
+            None => gdk_pixbuf::Pixbuf::from_file_at_scale(image_dir().join(Path::new("unknown.png")), width, height, true)
+        };
+        pb = res.expect("Error loading file into pixbuf");
+        if let Ok(today_count) = select!(i64 "count(*) from sighting where animal_id = ? and date(seen_at, \"unixepoch\", \"localtime\") = date(\"now\")",
+                animal_id) {
+            if today_count > 0 {
+                add_tick(&pb);
+            }
+        }
+    }
+    pb
+}
+
+fn add_tick(dest_pb: &gdk_pixbuf::Pixbuf) {
     let tick_pb = gdk_pixbuf::Pixbuf::from_file(&image_dir().join(Path::new("tick.png")))
         .expect("Can't load tick image.");
-    let dest_pb = image.get_pixbuf().unwrap();
     let dw = dest_pb.get_width();
     let dh = dest_pb.get_height();
     let tw = tick_pb.get_width();
@@ -190,7 +194,6 @@ fn add_tick(image: &gtk::Image) {
         gdk_pixbuf::InterpType::Bilinear,
         255,
     );
-    image.set_from_pixbuf(Some(&dest_pb));
 }
 
 pub fn refresh_images(vbox: &gtk::Box) -> glib::Continue {
